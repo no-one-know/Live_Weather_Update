1697eb584ea89f5ab1f970e0a3e5b33590d3dcae
Chetan Bagra <bagra200chetan@gmail.com>
2023-06-03 10:39:11 +0530

    final_push

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const form = document.getElementById("locationInput");
 const search = document.querySelector(".search");
 const btn = document.querySelector(".submit");
 const cities = document.querySelectorAll(".city");
-const key = '2642cf82c935847d536d202f05ad9369';
+const key = '450846123b0b4f4986c33757230306';
 
 // Function to get user's current location
 function getCurrentLocation() {
@@ -32,7 +32,7 @@ function handleLocationError(error) {
 function getWeatherByLocation(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${latitude},${longitude}&aqi=yes`;
   fetch(url)
     .then(response => {
       if (response.ok) {
@@ -52,38 +52,76 @@ function getWeatherByLocation(position) {
 
 // Function to update the weather information in the UI
 function updateWeatherUI(data) {
-  windOutput.innerHTML = data.wind.speed + " meter/sec";
-  cloudOutput.innerHTML = data.clouds.all + "%";
-  humidityOutput.innerHTML = data.main.humidity + "%";
-  nameOutput.innerHTML = data.name;
-  temp.innerHTML = Math.round(data.main.temp - 275.15) + "&#176;";
-  conditionOutput.innerHTML = data.weather[0].main;
+    windOutput.innerHTML = data.current.wind_kph + " km/hr";
+    cloudOutput.innerHTML = data.current.cloud + "%";
+    humidityOutput.innerHTML = data.current.humidity + "%";
+    nameOutput.innerHTML = data.location.name;
+    temp.innerHTML = data.current.temp_c + "&#176;";
+    conditionOutput.innerHTML = data.current.condition.text;
 
-  const localDate = new Date((data.sys.sunrise + data.timezone) * 1000);
-  const localYear = localDate.getFullYear();
-  const localMonth = localDate.getMonth() + 1;
-  const localDay = localDate.getDate();
-  const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const w = weekDay[localDate.getDay()];
+    const date=data.location.localtime;
+    const y=parseInt(date.substr(0,4));
+    const m=parseInt(date.substr(5,2));
+    const d=parseInt(date.substr(8,2));
+    const time=date.substr(11);
+        
+    
+     const w=dayOfTheWeek(d,m,y);
+    
+    dateOutput.innerHTML=`${w} ${d}-${m}-${y}`;
+    timeOutput.innerHTML=time;
 
-  dateOutput.innerHTML = `${w} ${localDay}-${localMonth}-${localYear}`;
+    const iconId=data.current.condition.icon.substr("//cdn.weatherapi.com/weather/64x64".length);
+    icon.src="./icons" + iconId ;
 
-  const localHours = localDate.getHours();
-  const localMinutes = localDate.getMinutes();
+    let timeOfDay="day";
+         const code=data.current.condition.code;
 
-  const hoursString = String(localHours).padStart(2, '0');
-  const minutesString = String(localMinutes).padStart(2, '0');
 
-  timeOutput.innerHTML = `${hoursString}:${minutesString}`;
+         if(!data.current.is_day){
+            timeOfDay="Night";
+         }
 
-  let timing = undefined;
-  if (localHours >= 19 || localHours < 6) {
-    timing = 'night';
-  } else if (localHours >= 6 || localHours < 19) {
-    timing = 'day';
-  }
-  icon.src = `./icons/${timing}/${data.weather[0].main}.png`;
-  app.style.backgroundImage = `url("./images/${timing}/${data.weather[0].main}.jpg")`;
+         if(code==1000){
+            app.style.backgroundImage=`url("./images/${timeOfDay}/clear.jpg")`;
+
+            btn.style.background="#e5ba92";
+            if(timeOfDay=="Night"){
+                btn.style.background="#181e27";
+            }
+
+         }
+
+        else if(code==1003 || code==1006 || code==1009 || code==1030 || code==1069 || code==1087 
+            || code==1135 ||code==1273 ||code==1276 || code== 1279 || code==1282)
+            {   
+                app.style.backgroundImage=`url("./images/${timeOfDay}/cloudy.jpg")`;
+                btn.style.background="#fa6d1b";
+                if(timeOfDay=="Night"){
+                    btn.style.background="#181e27";
+                }
+            }
+
+            else if(code==1063 || code==1069 || code==1072 || code==1050 || code==1153 || code==1180 
+                || code==1183 ||code==1186 ||code==1189 || code== 1192 || code==1195 || code==1204 
+                || code==1207 || code==1240 || code==1243 || code==1246 || code==1249 || code==1252)
+                {  
+                    app.style.backgroundImage=`url("./images/${timeOfDay}/rainy.jpg")`;
+                    btn.style.background="#647d75";
+                    if(timeOfDay=="Night"){
+                        btn.style.background="#325c80";
+                    }
+                }
+
+                else{
+                
+                    app.style.backgroundImage=`url("./images/${timeOfDay}/snony.jpg")`;
+                    btn.style.background="#647d75";
+                    if(timeOfDay=="Night"){
+                        btn.style.background="#1b11b1";
+                    }
+
+                }
 }
 
 // Event listener for city selection
@@ -104,9 +142,15 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
+
+function dayOfTheWeek(day,month,year){
+  const weekDay=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  return weekDay[new Date().getDay(day,month,year)] ;
+};
+
 // Function to get weather data by city name
 function getWeatherData(cityName) {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
+  let url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${cityName}&aqi=yes`;
   fetch(url)
     .then(response => {
       if (response.ok) {
